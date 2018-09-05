6cf7d634963151bf3a6b77ff54a9f85b2aff68e4
aemann01 <aemann01@ou.edu>
2018-09-05 13:42:21 -0700

    Added argument parser

--- bubble_tree.py
+++ bubble_tree.py
@@ -1,47 +1,49 @@
+#!/usr/bin/env python3
+
+'''This script draws a sample abundance bubble chart ordered by the leaves on a phylogenetic tree. Samples are ordered and colored by a metadata category. Useage: python3 bubble_tree.py -b <biom.txt> -m <mapping.txt> -t <tree.newick> -c <category>'''
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 import seaborn as sns
 import matplotlib.patches as mpatches
+import argparse
 from Bio import Phylo
 from itertools import repeat
 
-##ADD ARGUMENT PARSER HERE
+parser = argparse.ArgumentParser()
+requireparser = parser.add_argument_group('required arguments')
 
+requireparser.add_argument('-b', '--biom', help='Absolute abundance biom table. Must be tsv formatted.', required=True)
+requireparser.add_argument('-t', '--tree', help='Newick formatted tree', required=True)
+requireparser.add_argument('-m', '--map', help='Mapping file with metadata corresponding to samples. Must be tsv formatted', required=True)
+requireparser.add_argument('-c', '--category', help='Column category from mapping file to order/color samples by', required=True)
+parser.add_argument('-f', '--treeformat', help='Optional: set tree format type. Default is newick formatted tree.', default='newick')
+
+args = parser.parse_args()
+
+##TO DO: AT SOME POINT, ADD ALIGNMENT AND DISTANCE MATRIX OPTIONS?
+
+###LOAD FILES###
 #load in tab separated biom table -- must have hashed out first row
-biom = pd.read_csv("test_biom.txt", sep="\t", skiprows=1)
-
+biom = pd.read_csv(args.biom, sep="\t", skiprows=1)
+print("Reading %s as tsv formatted biom file" % args.biom)
 #load in newick format tree
-tree = Phylo.read("test_newick.tre", "newick")
-
+tree = Phylo.read(args.tree, args.treeformat)
+print("Reading %s as %s formatted tree file" % (args.tree, args.treeformat))
 #load in mapping file
-metadat = pd.read_csv("test_map.txt", sep="\t")
+metadat = pd.read_csv(args.map, sep="\t")
+print("Reading in %s as mapping file\n" % args.map)
 
-#preview tree
-print("Tree preview:\n")
-##print("Number of leaves %i" % //////)
-
-Phylo.draw_ascii(tree)
-
-#get order of leaves from tree
-leaves = []
-for leaf in tree.get_terminals():
-	leaves.append(leaf.name)
-
-#pull nodes from biom and reorder by leaves in tree
-subset = biom.loc[biom['#OTU ID'].isin(leaves)].set_index('#OTU ID')
-ordered = subset.reindex(leaves)
-
-######NORMALIZE ACROSS ROWS
+###NORMALIZE COUNTS IN BIOM TABLE###
 #first get max and min value for each row and append to dataframe
 biom['rowmax'] = biom.max(axis=1)
 biom['rowmin'] = biom.min(axis=1)
 
 def norm(x,rmin,rmax):
 	xnorm = (x - rmin)/(rmax - rmin)
-	return xnorm
-
+	return xnorm*300
 normdf = pd.DataFrame()
 
 #save first column to append to norm dataframe
@@ -50,22 +52,38 @@ samps = list(biom['#OTU ID'][1:])
 for i in range(1, len(biom)):
 	normdata = biom.iloc[i][1:-2].apply(norm, args=(biom['rowmin'][i], biom['rowmax'][i])).fillna(0)
 	normdf = normdf.append(normdata)
-
 normdf.insert(loc=0, column="#OTU ID", value=samps)
 
-#####EXTRA GROUPING/COLOR OPTIONS
+###REORDER NORMALIZED BIOM TABLE BY BRANCH ORDER###
+#preview tree topology
+print("Tree preview:\n")
+Phylo.draw_ascii(tree)
+
+#plot the tree, save to pdf
+Phylo.draw(tree, do_show=False)
+plt.savefig("tree.pdf")
+
+#get order of leaves from tree
+leaves = []
+for leaf in tree.get_terminals():
+	leaves.append(leaf.name)
+
+#pull nodes from biom and reorder by leaves in tree
+subset = normdf.loc[normdf['#OTU ID'].isin(leaves)].set_index('#OTU ID')
+ordered = subset.reindex(leaves)
+
+###GROUP DATA BY METADATA CATEGORY###
 #group samples by metadata category in mapping file
-grouped = metadat.groupby('CatC')['SampleID'].apply(list)
+grouped = metadat.groupby(args.category)['SampleID'].apply(list)
 sampOrder = []
 for i in grouped:
 	sampOrder += i
 final = ordered.reindex(columns=sampOrder)
 
-#flatten the matrix to plot cleveland style dotplot
+###GENERATE CLEVELAND STYLE DOTPLOT AND PLOT###
+#flatten the matrix
 x,y = np.meshgrid(final.columns, final.index)
 
-#plot the bubble chart, save to pdf
-#plt.subplot(1,2,2)
 #get color scheme based on grouping category
 colmap = sns.color_palette()
 
@@ -77,7 +95,7 @@ for i in grouped:
 
 #get corresponding legend values (preserve order in color list)
 legendCol = sorted(set(col), key=lambda x: col.index(x)) 
-legendName = metadat.CatC.unique()
+legendName = metadat[args.category].unique()
 
 #generate legend
 legendGen = []
@@ -90,15 +108,13 @@ plt.scatter(x=x.flatten(), y=y.flatten(), s=final.values.flatten(), zorder=3, c=
 #flip y axis to match tree
 ax = plt.gca()
 ax.set_ylim(ax.get_ylim()[::-1])
-ax.grid(True)
+ax.grid(True, linestyle="dotted")
 plt.legend(handles=legendGen)
 plt.xticks(rotation=90)
+#plt.show()
 plt.savefig("bubble.pdf")
 
-#plot the tree, save to pdf
-#plt.subplot(1,2,1)
-Phylo.draw(tree)
-plt.savefig("tree.pdf")
+
 
 
 
