a19b78f04d3b99d8f01b2eb036a3693f39ea8ee9
aemann01 <aemann01@ou.edu>
2019-05-28 16:00:13 -0700

    updated script

--- bubble_tree.py
+++ bubble_tree.py
@@ -4,19 +4,19 @@
 Samples are ordered and colored by a metadata category. Normalized across the row from 0 to 1. 
 Useage: python3 bubble_tree.py -i biom.txt -m mapping.txt -t tree.newick -c category -d [heatmap|bubblechart] <-r False -p False>'''
 
-##TO DO: ADD OPTIONS FOR DIFFERENT TREE FORMATS
 ##TO DO: ALIGN TIP NAMES WITH DOTTED LINES NATIVELY
 ##TO DO: INLCUDE NATIVE ALIGNMENT/DISTANCE MATRIX BUILD?
+##TO DO: RECOGNIZE AND SKIP HASHED OUT FIRST ROW
 
 import argparse
 parser = argparse.ArgumentParser()
 requireparser = parser.add_argument_group('required arguments')
 
 requireparser.add_argument('-i', '--input', help='Absolute abundance biom table. Must be tsv formatted.', required=True)
-requireparser.add_argument('-t', '--tree', help='Newick formatted tree', required=True)
+requireparser.add_argument('-t', '--tree', help='Tree file', required=True)
 requireparser.add_argument('-m', '--map', help='Mapping file with metadata corresponding to samples. Must be tsv formatted', required=True)
 requireparser.add_argument('-c', '--category', help='Column category from mapping file to order/color samples by', required=True)
-requireparser.add_argument('-d', '--display', help='Display data as a heatmap or bubblechart', default="bubblechart")
+requireparser.add_argument('-d', '--display', help='Display data as a heatmap or bubble', default="bubble")
 
 parser.add_argument('-f', '--treeformat', help='Optional: set tree format type. Default is newick formatted tree.', default='newick')
 parser.add_argument('-r', '--remote', help='Set this option as True running on a remote cluster. Disables the automatic $DISPLAY environment varible used by matplotlib', type=bool, default='False')
@@ -32,20 +32,21 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import matplotlib.patches as mpatches
 import pandas as pd
-import networkx as nx
 import numpy as np
 import seaborn as sns
 import os
 from Bio import Phylo
 from itertools import repeat
 
-biom = pd.read_csv(args.input, sep="\t", skiprows=1) #load in tab separated biom table -- must have hashed out first row
-#print("Reading %s as tsv formatted biom file..." % args.input)
+biom = pd.read_csv(args.input, sep="\t") #load in tab separated biom table 
+#uncomment below for hashed out first row of file
+#biom = pd.read_csv(args.input, sep="\t", skiprows=1)
+print("Reading %s as tsv formatted biom file..." % args.input)
 tree = Phylo.read(args.tree, args.treeformat) #load in newick format tree
-#print("Reading %s as %s formatted tree file..." % (args.tree, args.treeformat))
+print("Reading %s as %s formatted tree file..." % (args.tree, args.treeformat))
 metadat = pd.read_csv(args.map, sep="\t") #load in mapping file
-#print("Reading in %s as mapping file...\n" % args.map)
-#print("Generating figure for %s metadata category..." % args.category)
+print("Reading in %s as mapping file...\n" % args.map)
+print("Generating figure for %s metadata category..." % args.category)
 
 def normbubble(x,rmin,rmax):
 	xnorm = (x - rmin)/(rmax - rmin)
@@ -59,33 +60,36 @@ def gennormbubble():
 	biom['rowmax'] = biom.max(axis=1) #first get max and min value for each row and append to dataframe
 	biom['rowmin'] = biom.min(axis=1)
 	normdf = pd.DataFrame()
-	samps = list(biom['#OTU ID'][1:]) #save first column to append to norm dataframe
+	samps = list(biom['OTUID'][1:]) #save first column to append to norm dataframe
 	for i in range(1, len(biom)):
 		normdata = biom.iloc[i][1:-2].apply(normbubble, args=(biom['rowmin'][i], biom['rowmax'][i])).fillna(0)
 		normdf = normdf.append(normdata)
-	normdf.insert(loc=0, column="#OTU ID", value=samps)
+	normdf.insert(loc=0, column="OTUID", value=samps)
 	reorderbiom(normdf)
 
 def gennormheat():
 	biom['rowmax'] = biom.max(axis=1) #first get max and min value for each row and append to dataframe
 	biom['rowmin'] = biom.min(axis=1)
 	normdf = pd.DataFrame()
-	samps = list(biom['#OTU ID'][1:]) #save first column to append to norm dataframe
+	samps = list(biom['OTUID'][1:]) #save first column to append to norm dataframe
 	for i in range(1, len(biom)):
-		normdata = biom.iloc[i][1:-2].apply(normheat, args=(biom['rowmin'][i], biom['rowmax'][i])).fillna(0)
+		normdata = biom.iloc[i][1:-2].apply(lambda x: np.log(x+1))
 		normdf = normdf.append(normdata)
-	normdf.insert(loc=0, column="#OTU ID", value=samps)
+		#normdata = biom.iloc[i][1:-2].apply(np.log(i)).fillna(0)
+		#normdata = biom.iloc[i][1:-2].apply(normheat, args=(biom['rowmin'][i], biom['rowmax'][i])).fillna(0)
+		#normdf = normdf.append(normdata)
+	normdf.insert(loc=0, column="OTUID", value=samps)
 	reorderbiom(normdf)
 
 def reorderbiom(normdf):
 	leaves = [] #get order of leaves from tree
 	for leaf in tree.get_terminals():
 		leaves.append(leaf.name)
-	subset = normdf.loc[normdf['#OTU ID'].isin(leaves)].set_index('#OTU ID') #pull nodes from biom and reorder by leaves in tree
+	subset = normdf.loc[normdf['OTUID'].isin(leaves)].set_index('OTUID') #pull nodes from biom and reorder by leaves in tree
 	ordered = subset.reindex(leaves)
 	#group samples by metadata category in mapping file
-	filtmeta = metadat[metadat['#SampleID'].isin(list(ordered.columns))] #first remove rows that are not in the biom file
-	grouped = filtmeta.groupby(args.category)['#SampleID'].apply(list)
+	filtmeta = metadat[metadat['SampleID'].isin(list(ordered.columns))] #first remove rows that are not in the biom file
+	grouped = filtmeta.groupby(args.category)['SampleID'].apply(list)
 	sampOrder = []
 	for i in grouped:
 		sampOrder += i
@@ -136,7 +140,7 @@ def bubble(final, grouped, filtmeta):
 	#plt.show() #turn on for testing only
 	plt.savefig('%s_bubblePlot.pdf' % args.category, bbox_inches='tight')
 
-##TO DO: MAKE THIS PRETTY SON
+##TO DO: MAKE THIS PRETTY
 def heat(final):
 		#set up plot aesthetics
 	gs = gridspec.GridSpec(1, 2, width_ratios=[0.5, 3]) 
@@ -155,7 +159,7 @@ def heat(final):
 	treeax.set_yticks([])
 	Phylo.draw(tree, axes=treeax, do_show=False)
 	sns.set(font_scale=1)
-	sns.heatmap(final, mask=False, cmap="YlGnBu")
+	sns.heatmap(final, mask=False, cmap="Accent", xticklabels=True, yticklabels=True, square=True)
 	#save
 	#plt.show() #turn on for testing only
 	plt.savefig('%s_heatPlot.pdf' % args.category, bbox_inches='tight')
